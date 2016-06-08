ce4f8d566242fe011fb3139daa51f05a1deedca4
Sean Hayes <sean_hayes02@hotmail.com>
2016-06-08 11:28:31 -0700

    fixed init.sh to make environment setup much easier

--- init.sh
+++ init.sh
@@ -6,9 +6,25 @@ apt-get install git vim python-pip
 pip install virtualenv virtualenvwrapper
 
 git clone https://github.com/hvyk/config.git
+if [ -f ~/.bashrc ]
+then
+    rm ~/.bashrc
+fi
 ln -s ~/config/.bashrc ~/.bashrc
+source ~/.bashrc
+
+if [ -f ~/.vimrc ]
+then
+    rm ~/.vimrc
+fi
+ln -s ~/config/.vim/.vimrc ~/.vimrc
+
+if [ -d ~/.vim ]
+then
+    rm -r ~/.vim
+fi
 ln -s ~/config/.vim ~/.vim
-ln -s ~/config/.vim/.vimrc ~/.vim/.vimrc
+
 ln -s ~/config/preactivate ~/.virtualenvs/preactivate
 ln -s ~/config/postactivate ~/.virtualenvs/postactivate
 
